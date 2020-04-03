676310aae4cb867159a7c78a08dc76da8e04dd40
krautbax <20214119+krautbax@users.noreply.github.com>
2020-04-02 20:56:53 -0400

    Simple SB test; IntelliJ CE warnings removed

--- src/main/java/org/github/krautbax/examples/gsrestservice/Greeting.java
+++ src/main/java/org/github/krautbax/examples/gsrestservice/Greeting.java
@@ -5,15 +5,18 @@ public class Greeting {
     private final long id;
     private final String content;
 
+    @SuppressWarnings("WeakerAccess")
     public Greeting(long id, String content) {
         this.id = id;
         this.content = content;
     }
 
+    @SuppressWarnings("unused")
     public long getId() {
         return id;
     }
 
+    @SuppressWarnings("unused")
     public String getContent() {
         return content;
     }
---
+++ src/test/java/org/github/krautbax/examples/gsrestservice/ApplicationTest.java
@@ -0,0 +1,35 @@
+package org.github.krautbax.examples.gsrestservice;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.reactive.server.WebTestClient;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class ApplicationTest {
+
+    @Rule
+    public ExpectedException thrown = ExpectedException.none();
+
+    @Autowired
+    @SuppressWarnings("unused")
+    private WebTestClient webClient;
+
+    @Test
+    public void testActuator() {
+        webClient.get().uri(builder -> builder.path("/actuator/health").build()).exchange().expectStatus().isOk().expectBody()
+            .jsonPath("$.status").isEqualTo("UP");
+    }
+
+    @Test
+    public void testGreeting() {
+        webClient.get().uri(builder -> builder.path("/greeting").build()).exchange().expectStatus().isOk().expectBody()
+            .jsonPath("$.id").isEqualTo(1)
+            .jsonPath("$.content").isEqualTo("Hello, World!");
+    }
+}
