ecf41390fe3a73461d8bd721fc194356bfefa634
krautbax <20214119+krautbax@users.noreply.github.com>
2020-04-10 13:24:45 -0400

    Added Apache 2.0 license headers

--- src/test/java/org/github/krautbax/examples/gsrestservice/ApplicationTest.java
+++ src/test/java/org/github/krautbax/examples/gsrestservice/ApplicationTest.java
@@ -1,3 +1,9 @@
+/* 
+ * Copyright (c) 2020 krautbax.
+ * Licensed under the Apache License, Version 2.0
+ * http://www.apache.org/licenses/LICENSE-2.0
+ */
+
 package com.github.krautbax.examples.gsrestservice;
 
 import org.junit.Rule;
