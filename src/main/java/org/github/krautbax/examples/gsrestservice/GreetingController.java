a668236c272ffaf0a522fe0220096d72989c396f
Jason Gottschalk <20214119+krautbax@users.noreply.github.com>
2020-04-09 23:15:38 -0400

    com.github...

--- src/main/java/org/github/krautbax/examples/gsrestservice/GreetingController.java
+++ src/main/java/org/github/krautbax/examples/gsrestservice/GreetingController.java
@@ -1,4 +1,4 @@
-package org.github.krautbax.examples.gsrestservice;
+package com.github.krautbax.examples.gsrestservice;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
